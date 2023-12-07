09f45bf7d5905447c42ed54a1cb0a4601bdd5cac
tomoya0x03a <yamada0x03a@gmail.com>
2023-12-07 11:58:23 +0900

    線形探索する関数をリファクタリング。

--- linear_search.py
+++ linear_search.py
@@ -1,14 +1,34 @@
-# 線形探索法
+import unittest
+
 def linear_search(values, target):
+    """リスト内で目標値を線形探索する関数。
+
+    Args:
+        values (list): 探索対象のリスト。
+        target: 探したい値。
+
+    Returns:
+        int or None: 見つかった場合はインデックス、見つからない場合はNone。
+    """
     for i, value in enumerate(values):
         if value == target:
             return i
-    # 見つからない場合はNoneを返す。
+    return None
 
-def test_linear_search():
-    values = [7,4,2,8,10,9,1,5,3,6]
-    print(linear_search(values, 8) == 3)
-    print(linear_search(values, 11) == None)
+class TestLinearSearch(unittest.TestCase):
+    def setUp(self):
+        # テスト用のデータセット
+        self.values = [7, 4, 2, 8, 10, 9, 1, 5, 3, 6]
+
+    def test_existing_value(self):
+        # 存在する値の場合のテスト
+        result = linear_search(self.values, 8)
+        self.assertEqual(result, 3)
+
+    def test_non_existing_value(self):
+        # 存在しない値の場合のテスト
+        result = linear_search(self.values, 11)
+        self.assertIsNone(result)
 
 if __name__ == '__main__':
-    test_linear_search()
+    unittest.main()
