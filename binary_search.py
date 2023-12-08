fce3a1a6e10abb797a2d8e5ff1339927ca5264a4
tomoya0x03a <yamada0x03a@gmail.com>
2023-12-08 14:15:24 +0900

    ２分探索法のアルゴリズムを作成した。

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,23 @@
 # 2分探索法
 def binary_search(values, target):
-    None
+    lo = 0
+    hi = len(values) - 1
+
+    while lo <= hi:
+        mid = int((lo + hi) / 2)
+        if values[mid] == target:
+            return mid
+        elif values[mid] > target:
+            hi = mid -1 
+        elif values[mid] < target:
+            lo = mid + 1
+    return None
 
 def test_binary_search():
     values = [1, 4, 7, 9, 10, 16]
-    print(binary_search(values, 0) == 1)
-    print(binary_search(values, 3) == 9)
-    print(binary_search(values, 5) == 16)
+    print(binary_search(values, 1) == 0)
+    print(binary_search(values, 9) == 3)
+    print(binary_search(values, 16) == 5)
     print(binary_search(values, 8) == None)
 
 if __name__ == '__main__':
