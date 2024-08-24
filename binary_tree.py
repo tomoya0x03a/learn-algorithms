4c53f16abc780deed58a7291947f2e0b8d615434
tomoya0x03a <yamada0x03a@gmail.com>
2024-08-24 10:36:09 +0900

    コメントを追加した。

--- binary_tree.py
+++ binary_tree.py
@@ -5,26 +5,58 @@ Node = tuple[Optional[int], Optional[int], int]
 Nodes = list[Node]
 
 class BinaryTree():
+    """
+    二分木を表現するクラス。
+    
+    Attributes:
+        nodes (list): ノードのリスト。各ノードは (左の子ノードのインデックス, 右の子ノードのインデックス, 値) のタプルです。
+        
+    Methods:
+        traverse: 二分木の中間順走査を行い、値のリストを返す。
+    """
+    
     def __init__(self, nodes: Nodes) -> None:
+        """
+        コンストラクタ。二分木のノードのリストを受け取り、二分木を初期化します。
+        
+        Args:
+            nodes (list): ノードのリスト。各ノードは (左の子ノードのインデックス, 右の子ノードのインデックス, 値) のタプルです。
+        """
         self.nodes = nodes
 
-    def traverse(self):
+    def traverse(self) -> list[int]:
+        """
+        二分木の中間順走査を行い、値のリストを返す。
+        
+        Returns:
+            list: 中間順走査による値のリスト。
+        """
         def inorder_traversal(index: Optional[int]) -> list[int]:
             if index is None:
                 return []
-
+    
             left_child = self.nodes[index][0]
             right_child = self.nodes[index][1]
             value = self.nodes[index][2]
-
+    
             return inorder_traversal(left_child) + [value] + inorder_traversal(right_child)
-
+    
         return inorder_traversal(0)
 
 
 class TestBinaryTree(unittest.TestCase):
+    """
+    BinaryTree クラスのユニットテスト。
+    
+    Methods:
+        setUp: テストケースごとに共通のセットアップを行う。
+        test_traverse: traverse メソッドのテスト。
+    """
+    
     def setUp(self):
-        # テスト用のデータセット
+        """
+        テスト用のデータセットを準備する。
+        """
         self.nodes = [
             (1, 2, 10),
             (3, 4, 5),
@@ -35,10 +67,13 @@ class TestBinaryTree(unittest.TestCase):
             (None, None, 6),
             (None, None, 9),
         ]
-
+    
     def test_traverse(self):
+        """
+        traverse メソッドが正しく中間順走査を行い、期待される値のリストを返すことを確認する。
+        """
         expected_values = [2, 5, 6, 8, 9, 10, 11, 12]
-
+    
         binary_tree = BinaryTree(self.nodes)
         self.assertEqual(binary_tree.traverse(), expected_values)
 
